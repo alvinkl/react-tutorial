5db0b1f0d3f11d906e704281f9ada0de53664d29
Alvin Lim <alvink77@gmail.com>
2017-08-02 20:36:52 +0700

    first init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class TodoList extends Component {
+  render() {
+    return <h1>Hey Welcome to React Introduction Class!!</h1>
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
